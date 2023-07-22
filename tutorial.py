65acb6f74932b8a3742929bd4c3539d0b190e6a2
Le Ngoc Nam <yo231269@gmail.com>
2023-07-22 18:28:41 +0700

    Create a Background

--- tutorial.py
+++ tutorial.py
@@ -8,7 +8,6 @@ pygame.init()
 
 pygame.display.set_caption("Platformer")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = 60
 PLAYER_VEL = 5
@@ -16,8 +15,29 @@ PLAYER_VEL = 5
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Yellow.png")
 
     run = True
     while run:
@@ -27,6 +47,8 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(window, background, bg_image)
     pygame.quit()
     quit()
 
